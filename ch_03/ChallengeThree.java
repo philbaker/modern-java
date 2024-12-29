68fe768ce9a72a1924793e20b5ab7d14384ff6b3
philbaker <philbaker@users.noreply.github.com>
2024-12-29 22:42:58 +0000

    initial commit

---
+++ ch_03/ChallengeThree.java
@@ -0,0 +1,8 @@
+public class ChallengeThree {
+    public static void main(String[] args) {
+        System.out.println("Apple");
+        System.out.println("Banana");
+        System.out.println("Clementine");
+        System.out.println("Durian");
+    }
+}
---
+++ ch_04/LocalVariables.java
@@ -0,0 +1,6 @@
+public class LocalVariables {
+    public static void main(String[] args) {
+        String boss = "Jaqueline";
+        System.out.println(boss);
+    }
+}
