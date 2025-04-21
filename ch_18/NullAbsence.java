d6a4234f29819d877a24aac4dc323bead9b2001e
philbaker <phil@philbaker.me>
2025-04-21 06:12:07 +0100

    Add chapter 18

---
+++ ch_18/NullAbsence.java
@@ -0,0 +1,9 @@
+public class NullAbsence {
+    void main() {
+        String firstName = "Cher";
+        String lastName = null;
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+    }
+}
