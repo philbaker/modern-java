a9332f8c3a970d4890291b7e92feee4e1e175000
philbaker <phil@philbaker.me>
2025-04-22 07:02:29 +0100

    Move chapters

--- ch_19/Challenges.java
+++ ch_19/Challenges.java
@@ -1,88 +1,102 @@
 public class Challenges {
-    // int compute(int x) {
-    //     if (x == 0) {
-    //         return null;
-    //     }
-    //     else {
-    //         return x * x;
-    //     }
-    // }
+    void printNames(String[] names) {
+        if (names == null || names.length == 0) {
+            System.out.println("No names provided");
 
-    void challengeOne() {
-        // System.out.println(compute(5));
-        // Error because compute can return null
+            return;
+        }
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
     }
 
-    void printDistances(Integer[] distances) {
-        if (distances == null) {
-            distances = new Integer[] {};
-        }
-
-        for (int i = 0; i < distances.length; i++) {
-            System.out.println(distances[i] + " kilometers");
-        }
+    void challengeOne() {
+        printNames(new String[] {
+            "Joker",
+            "Batman",
+            "Alfred",
+        });
     }
 
     void challengeTwo() {
-        printDistances(new Integer[] { 1, 2 , 3 });
-        printDistances(null);
-    }
-
-    Integer onlyPositive(int x) {
-        if (x > 0) {
-            return x;
-        }
-        else {
-            return null;
-        }
+        printNames(null);
     }
 
     void challengeThree() {
-        System.out.println(
-            onlyPositive(45)
-        );
-        // 45
+        String[] jobs = new String[] {
+            "Carpenter",
+            "Baker",
+            null,
+            "Astronomer",
+        };
 
-        System.out.println(
-            onlyPositive(45 + 1)
-        );
-        // 46
-
-        System.out.println(
-            onlyPositive(0)
-        );
-        // null
-
-        System.out.println(
-            onlyPositive(-1)
-        );
-        // null
+        // This works because String can be null
+        for (int i = 0; i < jobs.length; i++) {
+            System.out.println(jobs[i]);
+        }
     }
 
     void challengeFour() {
-        int ducks = 5;
-        Integer sparrows = 3;
+        // Not valid because an int cannot be null
 
-        int birds = ducks + sparrows;
-        System.out.println(birds);
-        // 8
+        // int[] numbers = new int[] {
+        //     45,
+        //     32,
+        //     null,
+        //     94
+        // }
+        //
+        // for (int i = 0; i < numbers.length; i++) {
+        //     System.out.println(numbers[i]);
+        // }
+    }
 
-        // char[] face = new char[] { ':', ')' };
-        // Character[] smile = face;
+    String bigness(String letters) {
+        if (letters == null) {
+            return "No letters provided";
+        }
 
-        // System.out.println(smile);
-        // This won't work due to assigning a Character[] to a char[]
+        int bigness = 0;
+
+        for (int i = 0; i < letters.length(); i++) {
+            bigness++;
+        }
+
+        if (bigness < 5) {
+            return "small";
+        }
+        else if (bigness < 10) {
+            return "medium";
+        }
+        else {
+            return "big";
+        }
     }
 
     void challengeFive() {
-        char[] face = new char[] { ':', ')' };
+        System.out.println(
+            bigness("bore")
+        );
 
-        Character[] smile = new Character[face.length];
-        for (int i = 0; i < face.length; i++) {
-            smile[i] = face[i];
-        }
+        System.out.println(
+            bigness("boiler")
+        );
 
-        System.out.println(smile);
-        // [Ljava.lang.Character;@1990a65e
+        System.out.println(
+            bigness("filter")
+        );
+
+        System.out.println(
+            bigness("knower")
+        );
+
+        System.out.println(
+            bigness("chrysanthemum")
+        );
+
+        System.out.println(
+            bigness(null)
+        );
     }
 }
